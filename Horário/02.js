38eb6d730ca56b4dd3b1f14456a091d4f63b9102
IrPedro <99945573+irpedro@users.noreply.github.com>
2022-03-24 22:59:27 -0300

    Add files via upload

--- Horário/02.js
+++ Horário/02.js
@@ -1,1 +1,20 @@
-
+function tema() {
+	/* Tempo */
+	var dia = new Date()
+	var horas = dia.getHours()
+	var tempo = document.getElementById('tempo')
+	tempo.innerHTML = `Agora são ${horas} horas`
+	/* Outros */
+	var imagem = document.getElementById('imagem')
+	var fundo = document.getElementById('fundo')
+	if(horas > 0 && horas < 12){
+		imagem.src = 'manha.jpg'
+  document.body.style.background = '#e2cd9f'
+	} else if(horas >= 12 && horas <= 18){
+		imagem.src = 'tarde.jpg'
+   document.body.style.background = '#b9846f'
+	} else{
+		imagem.src = 'noite.jpg'
+   document.body.style.background = '#515154'
+	}
+}
