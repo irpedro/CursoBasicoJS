b40bf83c723a4a8928cc2abe37f125a31f05c4b8
IrPedro <99945573+irpedro@users.noreply.github.com>
2022-03-24 22:56:55 -0300

    Add files via upload

--- Gerador de Tabuadas/1.js
+++ Gerador de Tabuadas/1.js
@@ -1,1 +1,19 @@
-
+function tabu() {
+    var n = document.getElementById('num').valueAsNumber
+    if (n == "undefined") {
+        window.alert('Por favor digite um número')
+    } else {
+        tab.innerHTML = ''
+        for (var c = 0; c <= 10; c++) {
+            let item = document.createElement('option') 
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `${c}`
+            item.selected = 'selected'
+            tab.appendChild(item)
+        }
+    }
+}
+function limp(){
+    document.getElementById('num').value = ''
+    tab.innerHTML = ''
+}
