6fb3aacc6dbcd2bd9a3a96a88cfdaccb74b4ee90
IrPedro <99945573+irpedro@users.noreply.github.com>
2022-03-24 22:51:45 -0300

    Add files via upload

--- Contador/js.js
+++ Contador/js.js
@@ -1,1 +1,36 @@
+function calc() {
+    function rep() {
+        if (n < f) {
+            while (n <= f) {
+                res.innerHTML += `${n}, `
+                n += int
+            }
+        } else if (n > f) {
+            while (n >= f) {
+                res.innerHTML += `${n}, `
+                n -= int
+            }
+        } else {
+            res.innerHTML = 'Impossível contar'
+        }
+    }
 
+    n = document.getElementById('ini').valueAsNumber
+    var f = document.getElementById('fim').valueAsNumber
+    var int = document.getElementById('int').valueAsNumber
+    var res = document.getElementById('res')
+    res.innerHTML = 'Contando...<br>'
+    if (int == 0) {
+        window.alert('Passo INVÁLIDO considerando PASSO 1')
+        int = 1
+        rep()
+    } else {
+        rep()
+    }
+}
+function limp() {
+    document.getElementById('ini').value = ''
+    document.getElementById('fim').value = ''
+    document.getElementById('int').value = ''
+    document.getElementById('res').innerHTML = 'Insira os valores'
+}
